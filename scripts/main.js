60219279049c366b35f014bdb962801d633e918d
zhoujiayingvana <1147083244@qq.com>
2020-02-03 00:26:11 +0800

    :lipstick: updating index.html

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+var car = { manyCars: {a: "", "b": "Jeep"}, 7: "Mazda" };
+console.log(123!=="123");
+document.querySelector('#linux-net').onclick = function() {
+    alert('更多笔记正在玩命上传ing');
+}
+document.querySelector('#io-assemble').onclick = function() {
+    alert('更多笔记正在玩命上传ing');
+}
+document.querySelector('#io-interface').onclick = function() {
+    alert('更多笔记正在玩命上传ing');
+}
