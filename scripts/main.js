c0bb5c5b72a412ab5b74c806814236db80a1dcf1
zhoujiayingvana <1147083244@qq.com>
2020-02-03 17:16:36 +0800

    :lipstick: Update index UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,40 @@
-var car = { manyCars: {a: "", "b": "Jeep"}, 7: "Mazda" };
-console.log(123!=="123");
-document.querySelector('#linux-net').onclick = function() {
+document.querySelector('#linux-net').onclick = function () {
     alert('更多笔记正在玩命上传ing');
 }
-document.querySelector('#io-assemble').onclick = function() {
+document.querySelector('#io-assemble').onclick = function () {
     alert('更多笔记正在玩命上传ing');
 }
-document.querySelector('#io-interface').onclick = function() {
+document.querySelector('#io-interface').onclick = function () {
     alert('更多笔记正在玩命上传ing');
+}
+let myImage = document.querySelector("#top-bar")
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/星尘斗士6.webp') {
+        myImage.setAttribute('src', 'images/星尘斗士2.webp');
+    } else {
+        myImage.setAttribute('src', 'images/星尘斗士6.webp');
+    }
+}
+
+let subTitle = document.querySelector('#sub-title');
+let myButton = document.querySelector('button');
+function setUserName() {
+    let userName = prompt("请输入你的名字：");
+    if(!userName || userName===null){
+        alert("请输入一个名字！")
+        setUserName();
+    }else{
+        localStorage.setItem('name', userName);
+        subTitle.textContent = "Welcome " + userName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    subTitle.textContent = "Welcome " + storedName;
+}
+myButton.onclick=function(){
+    setUserName();
 }
